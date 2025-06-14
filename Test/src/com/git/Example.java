702f01388809b49df6b0bc34210228d99cef93b3
nitinbsagar@gmail.com <nitinbsagar@gmail.com>
2025-06-14 22:31:52 +0530

    Adding new class for addition of Two Number

---
+++ Test/src/com/git/Example.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addtion is Two Number ="+c);
+	}
+
+}
